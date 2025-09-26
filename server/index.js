2a74a207d8c44ad769b9d48021264d12c72e4d90
LuisMario69 <alzateluismario128@gmail.com>
2025-09-26 13:45:44 -0500

    Se implementó la simulación básica de envío y recepción de mensajes en el script del lado del cliente.

--- server/index.js
+++ server/index.js
@@ -1,13 +1,26 @@
 import express from 'express'
 
+import { Server } from 'socket.io'
+import { createServer } from 'http'
+
 const port = process.env.PORT ?? 3000
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server)
 
-app.get('/', (req, res) => {
-  res.send('<h1>Primer paso para la creacion del chat</h1>')
+io.on('connection', (socket) => {
+  console.log('a user connected')
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
 })
 
-app.listen(port, () => {
+app.get('/', (req, res) => {
+  res.sendFile('./client/index.html', { root: '.' })
+})
+
+server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`)
 })
